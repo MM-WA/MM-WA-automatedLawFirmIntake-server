1ab49e6fd191f2ab0d916b5fd10a23748b1f4d8b
MM-WA <144456091+MM-WA@users.noreply.github.com>
2025-05-21 20:15:53 +0500

    change - API key changes

--- src/utils/chatGptConfig.js
+++ src/utils/chatGptConfig.js
@@ -1,32 +1,29 @@
 import OpenAI from "openai";
 
-// const openai = new OpenAI({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
 const openai = new OpenAI({
-  apiKey: process.env.GROQ_API_KEY,
-  baseURL: "https://api.groq.com/openai/v1",
+  apiKey: process.env.OPENAI_API_KEY,
 });
+// const openai = new OpenAI({
+//   apiKey: process.env.GROQ_API_KEY,
+//   baseURL: "https://api.groq.com/openai/v1",
+// });
 
 async function sendRequestToGPT(incomingMessages) {
   try {
-    // const response = await openai.chat.completions.create({
-    //   model: "gpt-3.5-turbo", // or "gpt-3.5-turbo"
-    //   messages: [
-    //     { role: "system", content: "You are a helpful assistant." },
-    //     { role: "user", content: "Tell me a joke." },
-    //   ],
-    //   temperature: 0.7,
-    // });
-
+    const response = await openai.chat.completions.create({
+      model: "gpt-4o", // or "gpt-3.5-turbo"
+      messages: incomingMessages,
+      temperature: 0.7,
+    });
     // console.log(response.choices[0].message.content);
 
-    const chatCompletion = await openai.chat.completions.create({
-      model: "llama3-70b-8192",
-      messages: incomingMessages
-    });
 
-    return chatCompletion.choices[0].message.content;
+    // const chatCompletion = await openai.chat.completions.create({
+    //   model: "llama3-70b-8192",
+    //   messages: incomingMessages
+    // });
+
+    return response.choices[0].message.content;
   } catch (error) {
     throw error;
   }
